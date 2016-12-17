ce90efb2216a1a6b270bcbc469608027dcfc0e4a
harsh <harshh272@gmail.com>
2016-12-17 17:22:38 +0530

    Added Heap Sort

---
+++ Sorting/Heap Sort/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "heapSort.h"
+
+#define SIZE 20000      //Not Working for higher SIZE --> Stack Overlow ? Possibly...
+typedef long long int lli;
+
+int main()
+{
+    srand(time(NULL));
+
+    int *arr = (int *)malloc(sizeof(int)*SIZE);
+    if(arr == NULL)
+    {
+        printf("Buffer Overflow!");
+        return 0;
+    }
+
+    int i;
+    for(i=0; i<SIZE; i++)
+    {
+        arr[i] = rand() % SIZE - SIZE/2;
+    }
+
+    lli start_s=clock();
+    heap_sort(arr,0,SIZE-1);
+    lli stop_s=clock();
+
+    for(i=0; i<SIZE; i++)
+    {
+        if(i%10 == 0)
+        {
+            //printf("\n");
+        }
+        //printf("%d\t",arr[i]);
+    }
+
+    free (arr);
+    printf("\n\nExecution Time : %f sec\n",(stop_s-start_s)/(double)(CLOCKS_PER_SEC));
+
+    return 0;
+}
